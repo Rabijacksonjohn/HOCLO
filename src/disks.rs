8f6b916bd8ea6b1027ad8b642e7e60e742a0d426
Rabi Jackson <rabijackson@Rabis-MacBook-Air.local>
2026-08-27 15:02:48 +0530

    FAT: find the disk space

--- src/disks.rs
+++ src/disks.rs
@@ -6,7 +6,26 @@ pub fn disk_space(){
 
     println!("----------------List of Disk-----------------");
     let disk = Disks::new_with_refreshed_list();
+
     for disk in &disk {
-        println!("{:?}",disk);
+        let name=disk.name().to_string_lossy();
+        let mount_point=disk.mount_point().display();
+	
+	//get disk space
+	let total_space = disk.total_space();
+    let available_space = disk.available_space();
+    let used_space = total_space - available_space ;
+    
+    //convert to gb
+    let total_gb = total_space as f64 /1024.0 /1024.0 /1024.0;
+    let ave_gb = available_space as f64 /1024.0 /1024.0 /1024.0;
+    let used_gb = used_space as f64 /1024.0 /1024.0 /1024.0;
+    
+    println!("Disk name         : {:?}",name);
+    println!("Disk mount point  : {:?}",mount_point);
+    println!("Disk total GB     : {:.2}",total_gb);
+    println!("Disk available GB : {:.2}",ave_gb);
+    println!("Disk used space   : {:.2}\n",used_gb);
+
     }
 }
